5cf717b00af2bbec34d99d8327760159ca8eca7d
Jeffrey <jeffreystevenson99@ou.edu>
2021-01-26 22:25:18 -0600

    Initial Files

---
+++ R/myf.R
@@ -0,0 +1,14 @@
+#' Square function
+#'
+#' Squares a given value
+#'
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+#' x=1:30; myf(x)
+myf=function(x){
+  x^2
+}
