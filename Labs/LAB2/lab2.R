dd36b5f7d4cdb87b9b9b1a71d1610cd42cdd2698
Jeffrey <jeffreystevenson99@ou.edu>
2021-02-05 00:34:50 -0600

    update

--- Labs/LAB2/lab2.R
+++ Labs/LAB2/lab2.R
@@ -2,7 +2,7 @@
 #This will be different on the lab computer and on your home computer
 dird="C:\\Users\\stew9983\\OneDrive - University of Oklahoma\\DATAxls\\"
 
-#my function to read data 
+#my function to read data
 myread=function(csv){
   fl=paste(dird,csv,sep="")
   read.table(fl,header=TRUE,sep=",")
@@ -13,7 +13,7 @@ mpg.df=myread("EPAGAS.csv")
 
 #I.df=read.table(file.choose(),header=TRUE,sep=",")
 
-# in RMD use 
+# in RMD use
 
 I.df = myread("IRONORE.csv")
 
@@ -39,18 +39,18 @@ z[abs(z)>=2 & abs(z)<=3]
 
 #Find the values of I which are possible outliers
  I[abs(z)>=2 & abs(z)<=3]
- 
- 
+
+
 
 # Plot the outliers in red and the possible outliers in blue
 mycol = ifelse(abs(z)>3, "Red",
-        ifelse(abs(z)>=2 &abs(z)<=3,"Blue", "Black"))  
+        ifelse(abs(z)>=2 &abs(z)<=3,"Blue", "Black"))
 
-#Plot points with different plotting shapes        
+#Plot points with different plotting shapes
 mypch = ifelse(abs(z)>3, 20,
         ifelse(abs(z)>=2 &abs(z)<=3,21, 22))
 
-#Use an installed library 
+#Use an installed library
 # You will need to install the "lattice" package -- unless using the Lab computers
 #In R go to Packages   -- select "install packages" choose a mirror and then the "lattice" package
 # Now issue the following commands
